7a87785528865e7ea6007238886157ed5258e148
PuneetPant <puneet.pant04@gmail.com>
2022-08-27 15:54:40 +0530

    Till lecture 5

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,24 @@
 
 import './App.css';
 import { Greet } from './components/Greet';
+import { Heading } from './components/HeadingChildren';
+import { Oscar } from './components/OscarComponent';
 import { PersonList } from './components/PersonListArrayOfObjects';
 import { Person } from './components/PersonObject';
+import { Status } from './components/StatusConditional';
+import { name, personList } from './constants';
 
 function App() {
-  const name = {
-    first: 'Bruce',
-    last: 'Wayne'
-  }
-  const personList = [
-    {
-      first: 'Clark',
-      last: 'Kent'
-    },
-    {
-      first: 'Peter',
-      last: 'Pan'
-    },
-    {
-      first: 'Robin',
-      last: 'Hood'
-    }
-  ]
   return (
     <div className="App">
       <Greet name='Lala' messages={20} isLoggedIn={true} />
       <Person name={name} />
       <PersonList personList={personList} />
+      <Status status='success' />
+      <Heading> SuperHero Movie </Heading>
+      <Oscar>
+        <Heading>SuperHero Movie</Heading>
+      </Oscar>
     </div>
   );
 }
---
+++ src/constants.ts
@@ -0,0 +1,18 @@
+export const name = {
+  first: 'Bruce',
+  last: 'Wayne'
+}
+export const personList = [
+  {
+    first: 'Clark',
+    last: 'Kent'
+  },
+  {
+    first: 'Peter',
+    last: 'Pan'
+  },
+  {
+    first: 'Robin',
+    last: 'Hood'
+  }
+]
